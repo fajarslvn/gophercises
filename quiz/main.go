17798d32f7850bc4b1260272e793a359e59d6782
fajarslvn <jarphish@gmail.com>
2021-12-18 17:16:22 +0700

    init

--- quiz/main.go
+++ quiz/main.go
@@ -35,15 +35,23 @@ func main() {
 
 	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correct := 0
+
+problemLoop:
 	for i, p := range problems {
-		select {
-		case <-timer.C:
-			fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
-			return
-		default:
-			fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+
+		answerCh := make(chan string)
+		go func() {
 			var answer string
 			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println()
+			break problemLoop
+		case answer := <-answerCh:
 			if answer == p.a {
 				correct++
 			}
